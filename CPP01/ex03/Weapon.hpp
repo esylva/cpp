a0f28bb6fa81f6e288dedee6b2ae5cbdfd3e5d54
esylva <82966354+esylva@users.noreply.github.com>
2022-04-16 08:47:49 +0300

    Update Weapon.hpp

--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -22,7 +22,8 @@ class Weapon
 		
 	public:
 		Weapon();
-		Weapon(std::string type);
+		Weapon(std::string _name);
+		Weapon(std::string _name, _type);
 		~Weapon();
 
 		std::string getType(void) const;
