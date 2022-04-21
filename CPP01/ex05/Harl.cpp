28a5024147baa9994988495cddaeb5190a05d082
esylva <adrial@yandex.ru>
2022-04-21 06:49:08 +0300

    ex05 skeleton

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esylva <esylva@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 06:32:45 by esylva            #+#    #+#             */
+/*   Updated: 2022/04/21 06:32:45 by esylva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+	Harl::Harl(){
+	}
+
+	Harl::~Harl(){
+	}
+
+	void Harl::debug(void){
+
+	}
+
+	void Harl::info(void){
+
+	}
+
+	void Harl::warning(void){
+
+	}
+
+	void Harl::error(void){
+
+	}
+
+
+	void complain( std::string level ){
+
+	}
+
+
+#endif
+
---
+++ CPP01/ex05/Harl.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esylva <esylva@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 06:32:45 by esylva            #+#    #+#             */
+/*   Updated: 2022/04/21 06:32:45 by esylva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+class Harl {
+
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+
+public:
+	Harl(){};
+	~Harl(){};
+	void complain( std::string level );
+}
+
+#endif
+
+// The goal of this exercise is to use pointers to member functions. This is not a
+// suggestion. Harl has to complain without using a forest of if/else if/else. It doesn’t think
+// twice!
+// Create and turn in tests to show that Harl complains a lot. You can use the example
+// comments.
