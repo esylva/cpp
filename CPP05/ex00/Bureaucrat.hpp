0b3c7f9dd50c6369ffc5b5fd726657a467d5a6e2
esylva <adrial@yandex.ru>
2022-04-26 07:17:41 +0300

    00

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -15,13 +15,35 @@
 
 class Bureaucrat {
 
-    private:
-        std::string const   _name;
-        int                 _grade;
-    public:
+private:
+std::string const	_name;
+		int			_grade;
+        class GradeTooLowException {
 
+		};
+		class GradeTooHighException {
+
+		};
+
+	public:
+
+		Bureaucrat();
+		Bureaucrat(std::string const name);
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(const Bureaucrat& copy);
+
+		Animal &operator=(const Animal &obj);
+
+		std::string const getName(void);
+		int const getGrade(void);
+		void setName(std::string const name);
+		void setGrade(int grade);
+		void incrementGrade(int grade);
+		void decrementGrade(int grade);
 
 };
+	std::ostream&	operator<<(std::ostream& o, const Bureaucrat& bureaucrat);
+
 
 #endif
 
@@ -29,4 +51,10 @@ class Bureaucrat {
 // grade).
 // Any attempt to instantiate a Bureaucrat using an invalid grade must throw an ex-
 // ception:
-// either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException.
+// // either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException.
+// You will provide getters for both these attributes: getName() and getGrade(). 
+// Implement also two member functions to increment or decrement the bureaucrat grade. If
+// the grade is out of range, both of them will throw the same exceptions as the constructor.
+// You will implement an overload of the insertion («) operator to print something like
+// (without the angle brackets):
+// <name>, bureaucrat grade <grade>.
