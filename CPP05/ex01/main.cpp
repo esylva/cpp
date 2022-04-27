fcee2d0e952f830575166d8e1fd35ea13e797db8
esylva <esylva@student.21-school.ru>
2022-04-27 10:01:11 +0300

    optimised tests

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esylva <esylva@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 11:56:23 by esylva            #+#    #+#             */
-/*   Updated: 2022/04/27 09:52:27 by esylva           ###   ########.fr       */
+/*   Updated: 2022/04/27 10:00:24 by esylva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int main(void)
 	std::cout << b2 << std::endl;
 
 	try {
-		std::cout << "Trying to sign form_149 by " << b1.getName() << std::endl;
+		std::cout << "Trying to sign Form N149 by " << b1.getName() << std::endl;
 		form_149.beSigned(b1);
 	}
 	catch(const std::exception& e) {
@@ -61,7 +61,7 @@ int main(void)
 	}
 
 	try {
-		std::cout << "Trying to sign form_149 by " << b2.getName() << std::endl;
+		std::cout << "Trying to sign Form N149 by " << b2.getName() << std::endl;
 		form_149.beSigned(b2);
 		std::cout << "Success!" << std::endl;
 	}
@@ -69,18 +69,14 @@ int main(void)
 		std::cerr << e.what() << std::endl;
 	}
 
-	try {
-		std::cout << "Trying to sign again form_16 by " << b2.getName() << std::endl;
-		form_16.beSigned(b2);
-	}
-	catch(const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+	std::cout << std::endl << b2.getName() << " trying to sign the Form N16" << std::endl;
+	b2.signForm(form_16);
+	
 	std::cout << "Incrementing "<< b2.getName() << " grade" << std::endl;
 	b2.incrementGrade();
 	std::cout << b2 << std::endl;
 
-	std::cout << b2.getName() << " trying again to sign the form_16" << std::endl;
+	std::cout << b2.getName() << " trying again to sign the Form N16" << std::endl;
 	b2.signForm(form_16);
 	
 	std::cout << form_16 << std::endl << std::endl;
