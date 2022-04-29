f788f5c79b2f7880f77ad8a732144bc362af41bb
esylva <esylva@student.21-school.ru>
2022-04-29 20:54:00 +0300

    ex01

---
+++ CPP06/ex01/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct Data {
+	int		value;
+	char 	 c;
+};
+
+uintptr_t serialize(Data* ptr) {
+    return(reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw) {
+    return(reinterpret_cast<Data *>(raw));
+}
+
+int main(void) {
+    Data        data;
+    uintptr_t   data_serialized;
+    Data        *data_deserialized;
+
+    data.value = 42;
+	data.c = 'S';
+    std::cout << "Data value: " << data.value << std::endl;
+	std::cout << "Data char: " << data.c << std::endl;
+	std::cout << "Data address: " << &data << std::endl;
+    data_serialized = serialize(&data);
+    std::cout << "Data serialized uintptr_t: " << data_serialized << std::endl;
+    data_deserialized = deserialize(data_serialized);
+    std::cout << "Data deserialized address: " << data_deserialized << std::endl;
+	std::cout << "Data value: " << data.value << std::endl;
+	std::cout << "Data char: " << data.c << std::endl;
+	data.value = 24;
+	data.c = 'K';
+	std::cout << "Data value: " << data.value << std::endl;
+	std::cout << "Data char: " << data.c << std::endl;
+    return 0;
+}
