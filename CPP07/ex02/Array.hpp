7cd55d8b4211b82c92c970abd9826686f9a16fd7
esylva <adrial@yandex.ru>
2022-04-30 10:45:15 +0300

    sc

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -14,6 +14,63 @@
 #define ARRAY_HPP
 #include <iostream>
 
+template<typename Type>
+class Array
+{
+	private:
+		Type *t;
+		unsigned int count;
+		class NoAllocateException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw() {return ("Segfault");}
+		};
+	public:
+		Array() { t = 0; count = 0;}
+		Array(unsigned int count)
+		{
+			unsigned int i;
+
+			i = 0;
+			t = new Type[count];
+			this->count = count;
+			while(i < count)
+			{
+				this->t[i] = 5;
+				i++;
+			}
+		}
+		Array(Array const &array)
+		{
+			unsigned int i;
+
+			i = 0;
+			t = new Type[array.count];
+			count = array.count;
+			while (i < count)
+			{
+				t[i] = array.t[i];
+				i++;
+			}
+		}
+		Type &operator[](unsigned int elem)
+		{
+			if (elem >= 0 && elem < count)
+			{
+				return (t[elem]);
+			}
+			throw NoAllocateException();
+			return (t[0]);
+		}
+		~Array() 
+		{
+			delete[]t;
+		}
+		int size()
+		{
+			return (count);
+		}
+};
 
 
 #endif
