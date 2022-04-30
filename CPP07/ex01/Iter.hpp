4c623bf90a25f93c74571fd118c508d24e3edf12
esylva <adrial@yandex.ru>
2022-04-30 10:41:51 +0300

    ex01

--- CPP07/ex01/Iter.hpp
+++ CPP07/ex01/Iter.hpp
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Array.hpp                                          :+:      :+:    :+:   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: esylva <esylva@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/30 10:01:49 by esylva            #+#    #+#             */
-/*   Updated: 2022/04/30 10:01:49 by esylva           ###   ########.fr       */
+/*   Created: 2022/04/30 10:01:02 by esylva            #+#    #+#             */
+/*   Updated: 2022/04/30 10:01:02 by esylva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
+#ifndef ITER_HPP
+#define ITER_HPP
 #include <iostream>
 
 
+template<typename T>
+void foo(T const &a) {
+	std::cout << a << std::endl;
+}
+
+
+template<typename T>
+void iter(T *array, int arrayLenth, void (*foo)(T const &)) {
+	for (int i = 0; i < arrayLenth; i++) {
+		foo(array[i]);
+	}
+}
 
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Iter.hpp"
+
+void ber(const int &i) {
+	std::cout << i <<std::endl;
+}
+
+void bar(const std::string &s) {
+	std::cout << s <<std::endl;
+}
+
+int main() {
+	std::string str[4] = {"Ехали", "медведи", "на", "велосипеде"};
+	int a[5] = {9, 7, 5, 3, 1};
+	iter(a, 5, ber);
+	std::cout << std::endl;
+	iter(str, 4, bar);
+	std::cout << std::endl;
+	iter(a, 5, foo);
+	std::cout << std::endl;
+	iter(str, 4, foo);	
+	return 0;
+}
