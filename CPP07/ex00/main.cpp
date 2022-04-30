cf34d89b5d828b382443ca585b6782cd6aa573d6
esylva <adrial@yandex.ru>
2022-04-30 09:50:41 +0300

    ex00

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -10,3 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main() {
+	int a = 2;
+	int b = 3;
+	std::string c = "String1";
+	std::string d = "String2";
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "SWAP!" << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "SWAP!" << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	
+	return 0;
+}
